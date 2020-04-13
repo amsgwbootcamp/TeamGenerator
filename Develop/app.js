947b82bb158656df3b5729ad7ab536288343a419
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-04-12 21:51:11 -0400

    Adding colors for different positions

--- Develop/app.js
+++ Develop/app.js
@@ -1,85 +1,163 @@
-const Manager = require("./manager");
-const Engineer = require("./engineer");
-const Intern = require("./intern");
-const Employee = require("./employee")
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const render = require("./htmlRenderer");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
-
+const path = require("path");
+const util = require("util");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+var counter = 1;
+var teamArray = [];
+var continueLoop = true;
 
-const render = require("./htmlRenderer");
-​
+const writeFileAsync = util.promisify(fs.writeFile);
+
 async function init() 
 {
-  try {
+  try 
+  {
     // user prompts
-    const { managerName } = await promptInitial();
-    console.log("You entered the following: " + managerName);    
-//    return writeFileAsync("Readme.md", generateReadme(githubUsername, projectTitle, description, installInfo, contributing, tests, license, usage, email));
+    const { managerName, managerEmail, officeNumber } = await promptInitial();
+    const manager = new Manager(managerName, counter, managerEmail, officeNumber);
+    teamArray.push(manager);
+    counter++;
+       
+    while (continueLoop)
+    {
+        const { option } = await promptChoice();
+        switch(option)
+        {
+            case "Engineer":
+                console.log("you choose Engineer");
+                const { engineerName, engineerEmail, userName } = await promptEngineer();
+                const engineer = new Engineer(engineerName, counter, engineerEmail, userName);
+                teamArray.push(engineer);
+                counter++;
+                break;
+        
+            case "Intern":
+                console.log("you choose Intern");
+                const { internName, internEmail, schoolName } = await promptIntern();
+                const intern = new Intern(internName, counter, internEmail, schoolName);
+                teamArray.push(intern);
+                counter++;
+                break;
+
+            default:
+                continueLoop = false;
+                break;    
+        }
+    }
+    const html = render.render(teamArray);
+    return writeFileAsync(outputPath, html);
   }
-  catch (err) {
+  catch (err) 
+  {
       console.log(err);
   }
 }  
 
- function promptInitial() {
-    return inquirer.prompt([
-      {
+function promptInitial() {
+    return inquirer.prompt
+    (
+      [
+        {
         message: "What is your manager's name?",  
         type: "input",
         name: "managerName",
         validate: function validateManagerName(managerName) 
-        {   return managerName !== "";  }}
-/*      ,
+        {   return managerName !== "";  }
+        },
         {
         type: "input",
-        name: "projectTitle",
-        message: "What is the title of your project?"
-        }
-        ,
-        {
-        type: "input",
-        name: "description",
-        message: "What is a brief description of your project?"
-        }
-        ,
-        {
-        type:"input",
-        name: "installInfo",
-        message: "Installation instructions"
+        name: "managerEmail",
+        message: "What is the manager's email?",
+        validate: function validateManagerEmail(managerEmail) 
+        { return managerEmail !== ""; }
         }
         ,
         {
         type: "input",
-        name: "contributing",
-        message: "Who has worked on this with you?"
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        validate: function validateOfficeNumber(officeNumber) 
+        { return officeNumber !== ""; }
         }
-        ,
-        {
-        type: "input",
-        name: "tests",
-        message: "Are there any tests for this project?"
-        }
-        ,
-        {
-        type: "checkbox",
-        message: "What licenses are required?",
-        name: "license",
-        choices: ["None",
-                  "Apache License 2.0",
-                  "GNU General Public License 3.0",
-                  "MIT License"
-                ]
-        }
-        ,
-        {
-        type: "input",
-        name: "usage",
-        message: "Enter the command to run this program"
-        }   */
-      ]);
-  }
+    ]);
+}   
 
-  init();
+function promptEngineer() {
+    return inquirer.prompt
+    (
+      [
+        {
+        message: "What is your engineer's name?",  
+        type: "input",
+        name: "engineerName",
+        validate: function validateEngineerName(engineerName) 
+        {   return engineerName !== "";  }
+        },
+        {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email?",
+        validate: function validateEngineerEmail(engineerEmail) 
+        {   return engineerEmail !== "";  }
+        }
+        ,
+        {
+        type: "input",
+        name: "userName",
+        message: "What is the engineer's GitHub Username?",
+        validate: function validateuserName(userName) 
+        {   return userName !== "";  }
+        }
+    ]);
+}   
+
+function promptIntern() {
+    return inquirer.prompt
+    (
+      [
+        {
+        message: "What is your intern's name?",  
+        type: "input",
+        name: "internName",
+        validate: function validateInternName(internName) 
+        {   return internName !== "";  }
+        },
+        {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email?",
+        validate: function validateInternEmail(internEmail) 
+        {   return internEmail !== "";  }
+        }
+        ,
+        {
+        type: "input",
+        name: "schoolName",
+        message: "What school does the intern attend?",
+        validate: function validateschoolName(schoolName) 
+        {   return schoolName !== "";  }
+        }
+    ]);
+}
+
+function promptChoice() {
+    return inquirer.prompt
+    (
+      [
+        {
+        message: "Who do you want to enter next?",  
+        type: "list",
+        name: "option",
+        choices: ["Engineer", "Intern", "I'm finished"]
+        }
+      ]
+    );
+}   
+
+init();
